d8cd7d32dfd44c7b3cedbbeae210f2905570a045
Joerg Burmeister <joerg.burmeister@agito-it.de>
2013-11-22 09:22:36 +0100

    fix invalid change access
    Change-Id: I22f1774668f2f0ebabc361f78d588d288c87e56f

--- src/main/java/org/agito/demo/mdm/material/MaterialBPMOController.java
+++ src/main/java/org/agito/demo/mdm/material/MaterialBPMOController.java
@@ -2,6 +2,20 @@ package org.agito.demo.mdm.material;
 
 // @@begin imports
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+import org.agito.demo.mdm.material.MaterialBPMOAccess.AlternativeUnitOfMeasures;
+import org.agito.demo.mdm.material.MaterialBPMOAccess.AlternativeUnitOfMeasures.Row;
+import org.agito.demo.mdm.material.MaterialBPMOAccess.BaseUnitOfMeasure;
+import org.agito.demo.mdm.material.dto.MaterialHeaderDTO;
+import org.agito.demo.mdm.material.dto.MaterialHeaderList;
+
 import de.agito.cps.core.annotations.BPMO;
 import de.agito.cps.core.annotations.Expression;
 import de.agito.cps.core.annotations.ExpressionDependency;
@@ -19,24 +33,6 @@ import de.agito.cps.core.bpmo.api.controller.BPMOController;
 import de.agito.cps.core.bpmo.api.controller.IBPMOControllerContext;
 import de.agito.cps.core.engine.runtime.BusinessLog;
 import de.agito.cps.core.utils.StringUtils;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.UUID;
-import org.agito.demo.mdm.material.MaterialBPMO;
-import org.agito.demo.mdm.material.MaterialBPMOAccess;
-import org.agito.demo.mdm.material.MaterialBPMOAccess.AlternativeUnitOfMeasures;
-import org.agito.demo.mdm.material.MaterialBPMOAccess.AlternativeUnitOfMeasures.Row;
-import org.agito.demo.mdm.material.MaterialBPMOAccess.BaseUnitOfMeasure;
-import org.agito.demo.mdm.material.MaterialBPMOAction;
-import org.agito.demo.mdm.material.MaterialBPMOLanguage;
-import org.agito.demo.mdm.material.MaterialBPMOLifecycle;
-import org.agito.demo.mdm.material.MaterialBPMOProcessActivity;
-import org.agito.demo.mdm.material.dto.MaterialHeaderDTO;
-import org.agito.demo.mdm.material.dto.MaterialHeaderList;
 
 // @@end
 
@@ -48,7 +44,9 @@ import org.agito.demo.mdm.material.dto.MaterialHeaderList;
  */
 // @@end
 @BPMO(id = "MaterialBPMO", version = "1.0.0", xml = "org/agito/demo/mdm/material/MaterialBPMO.bpmo")
-public class MaterialBPMOController extends BPMOController<MaterialBPMOAccess, MaterialBPMOAction, MaterialBPMOLifecycle, MaterialBPMOLanguage, MaterialBPMOProcessActivity, MaterialBPMO> {
+public class MaterialBPMOController
+		extends
+		BPMOController<MaterialBPMOAccess, MaterialBPMOAction, MaterialBPMOLifecycle, MaterialBPMOLanguage, MaterialBPMOProcessActivity, MaterialBPMO> {
 
 	public MaterialBPMOController(final IBPMOControllerContext context) {
 		super(context);
@@ -118,7 +116,8 @@ public class MaterialBPMOController extends BPMOController<MaterialBPMOAccess, M
 	// @@end
 	@Expression(artifact = "Header$AlternativeUnitOfMeasures$AlternativeUnitOfMeasure", type = ExpressionType.CELL_BASED_CONTROL)
 	@ExpressionDependency("Header$BaseUnitOfMeasure")
-	public void cpsControlAlternativeUnitOfMeasures$AlternativeUnitOfMeasure(final MaterialBPMOAccess bpmoAccess, final IControlAttributes controlAttributes, final AlternativeUnitOfMeasures.Row rowAccess) {
+	public void cpsControlAlternativeUnitOfMeasures$AlternativeUnitOfMeasure(final MaterialBPMOAccess bpmoAccess,
+			final IControlAttributes controlAttributes, final AlternativeUnitOfMeasures.Row rowAccess) {
 		final BaseUnitOfMeasure baseUnitOfMeasure = bpmoAccess.getBaseUnitOfMeasure();
 		/*
 		 * Should only editable if the value not equals BaseUnitOfMeasure
@@ -140,82 +139,94 @@ public class MaterialBPMOController extends BPMOController<MaterialBPMOAccess, M
 		BusinessLog businessLog;
 		switch (action) {
 		case ReadMaterial:
-			bpmoAccess.getBPMOData().clearMessages();
-			MaterialHeaderDTO material = (MaterialHeaderDTO) parameters.get(ActionParameter.MATERIAL_HEADER_DTO
-					.toString());
-			bpmoAccess.getName().setOriginalValue(material.getName());
-			bpmoAccess.getMaterialNumber().setOriginalValue(material.getNumber());
+			try {
+				// IgnoreControls to avoid Invalid value change access to read only element trigger by modifications of Elements with DeletionFlag=true
+				bpmoAccess.getBPMOData().setIgnoreControlsOnValueChange();
 
-			// +++ simulate a service interface to get more details of material +++
+				// clear all previous generated messages
+				bpmoAccess.getBPMOData().clearMessages();
+				MaterialHeaderDTO material = (MaterialHeaderDTO) parameters.get(ActionParameter.MATERIAL_HEADER_DTO
+						.toString());
+				bpmoAccess.getName().setOriginalValue(material.getName());
+				bpmoAccess.getMaterialNumber().setOriginalValue(material.getNumber());
 
-			// set same sample data
-			bpmoAccess.getMaterialType().setOriginalValue("1");
-			bpmoAccess.getGrossWeight().setOriginalValue("10");
-			bpmoAccess.getNetWeight().setOriginalValue("12");
-			bpmoAccess.getAllowedPackagingWeight().setOriginalValue("15");
-			bpmoAccess.getAllowedPackagingVolume().setOriginalValue("60");
-			bpmoAccess.getVolume().setOriginalValue("14");
-			bpmoAccess.getBaseUnitOfMeasure().setOriginalValue("KGM");
+				// +++ simulate a service interface to get more details of material +++
 
-			// add row for BaseUnitOfMeasure
-			Row row = bpmoAccess.getAlternativeUnitOfMeasures().createOriginalRow();
-			row.getAlternativeUnitOfMeasure().setValue(bpmoAccess.getBaseUnitOfMeasure().getOriginalValue().getKey());
-			row.getDenominatorConversion().setValue("1");
-			row.getNumeratorConversion().setValue("5");
-			bpmoAccess.getAlternativeUnitOfMeasures().addOriginalRow(row);
+				// set same sample data
+				bpmoAccess.getMaterialType().setOriginalValue("1");
+				bpmoAccess.getGrossWeight().setOriginalValue("10");
+				bpmoAccess.getNetWeight().setOriginalValue("12");
+				bpmoAccess.getAllowedPackagingWeight().setOriginalValue("15");
+				bpmoAccess.getAllowedPackagingVolume().setOriginalValue("60");
+				bpmoAccess.getVolume().setOriginalValue("14");
+				bpmoAccess.getBaseUnitOfMeasure().setOriginalValue("KGM");
 
-			// add further UnitOfMeasure
-			row = bpmoAccess.getAlternativeUnitOfMeasures().createOriginalRow();
-			row.getAlternativeUnitOfMeasure().setValue("GRM");
-			row.getDenominatorConversion().setValue("4");
-			row.getNumeratorConversion().setValue("6");
-			bpmoAccess.getAlternativeUnitOfMeasures().addOriginalRow(row);
+				// add row for BaseUnitOfMeasure
+				Row row = bpmoAccess.getAlternativeUnitOfMeasures().createOriginalRow();
+				row.getAlternativeUnitOfMeasure().setValue(
+						bpmoAccess.getBaseUnitOfMeasure().getOriginalValue().getKey());
+				row.getDenominatorConversion().setValue("1");
+				row.getNumeratorConversion().setValue("5");
+				bpmoAccess.getAlternativeUnitOfMeasures().addOriginalRow(row);
 
-			// add plant
-			MaterialBPMOAccess plant = bpmoAccess.getPlants().createAndAddElement("Berlin");
-			plant.getPlants$ProductionSupervisor().setOriginalValue("Supervisor a");
-			plant.getPlants$MinimumLotSize().setOriginalValue(new BigDecimal(1));
-			plant.getPlants$MaximumLotSize().setOriginalValue(new BigDecimal(5));
-			plant.setErasable(false);
+				// add further UnitOfMeasure
+				row = bpmoAccess.getAlternativeUnitOfMeasures().createOriginalRow();
+				row.getAlternativeUnitOfMeasure().setValue("GRM");
+				row.getDenominatorConversion().setValue("4");
+				row.getNumeratorConversion().setValue("6");
+				bpmoAccess.getAlternativeUnitOfMeasures().addOriginalRow(row);
 
-			// add storage location
-			MaterialBPMOAccess storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
-					plant.getPlants$StorageLocations().createStorageLocationId("0971"));
-			storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("done");
-			storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
-					plant.getPlants$StorageLocations().createStorageLocationId("0972"));
-			storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("none");
-			storageLocation.setErasable(false);
+				// add plant
+				MaterialBPMOAccess plant = bpmoAccess.getPlants().createAndAddElement("Berlin");
+				plant.getPlants$ProductionSupervisor().setOriginalValue("Supervisor a");
+				plant.getPlants$MinimumLotSize().setOriginalValue(new BigDecimal(1));
+				plant.getPlants$MaximumLotSize().setOriginalValue(new BigDecimal(5));
+				plant.setErasable(false);
 
-			// add plant
-			plant = bpmoAccess.getPlants().createAndAddElement("Munich");
-			plant.getPlants$ProductionSupervisor().setOriginalValue("Supervisor b");
-			plant.getPlants$MinimumLotSize().setOriginalValue(new BigDecimal(2));
-			plant.getPlants$MaximumLotSize().setOriginalValue(new BigDecimal(7));
-			plant.setErasable(false);
-			plant.getContext().setDeletionFlagOriginalValue();
+				// add storage location
+				MaterialBPMOAccess storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
+						plant.getPlants$StorageLocations().createStorageLocationId("0971"));
+				storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("done");
+				storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
+						plant.getPlants$StorageLocations().createStorageLocationId("0972"));
+				storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("none");
+				storageLocation.setErasable(false);
 
-			// add storage location
-			storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
-					plant.getPlants$StorageLocations().createStorageLocationId("1071"));
-			storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("done");
-			storageLocation.setErasable(false);
+				// add plant
+				plant = bpmoAccess.getPlants().createAndAddElement("Munich");
+				plant.getPlants$ProductionSupervisor().setOriginalValue("Supervisor b");
+				plant.getPlants$MinimumLotSize().setOriginalValue(new BigDecimal(2));
+				plant.getPlants$MaximumLotSize().setOriginalValue(new BigDecimal(7));
+				plant.setErasable(false);
+				plant.getContext().setDeletionFlagOriginalValue();
 
-			// add sales org
-			MaterialBPMOAccess salesOrg = bpmoAccess.getSalesOrganizations().createAndAddElement(
-					bpmoAccess.getSalesOrganizations().createSalesOrganization("8637"),
-					bpmoAccess.getSalesOrganizations().createDistributionChannel("98"));
-			salesOrg.getSalesOrganizations$StatisticsGroup().setOriginalValue("group 1");
-			salesOrg.getSalesOrganizations$MinimumOrderQuantity().setOriginalValue("3");
-			salesOrg.getSalesOrganizations$MinimumDeliveryQuantity().setOriginalValue("9");
-			salesOrg.setErasable(false);
+				// add storage location
+				storageLocation = plant.getPlants$StorageLocations().createAndAddElement(
+						plant.getPlants$StorageLocations().createStorageLocationId("1071"));
+				storageLocation.getPlants$StorageLocations$StockInQualityInspection().setOriginalValue("done");
+				storageLocation.setErasable(false);
 
-			org.agito.demo.mdm.material.MaterialBPMOAccess.SalesOrganizations.SalesTexts.Row rowTexts = salesOrg
-					.getSalesOrganizations$SalesTexts().createOriginalRow();
-			rowTexts.getLanguage().setValue("en");
-			rowTexts.getText().setValue("text en");
-			salesOrg.getSalesOrganizations$SalesTexts().addOriginalRow(rowTexts);
+				// add sales org
+				MaterialBPMOAccess salesOrg = bpmoAccess.getSalesOrganizations().createAndAddElement(
+						bpmoAccess.getSalesOrganizations().createSalesOrganization("8637"),
+						bpmoAccess.getSalesOrganizations().createDistributionChannel("98"));
+				salesOrg.getSalesOrganizations$StatisticsGroup().setOriginalValue("group 1");
+				salesOrg.getSalesOrganizations$MinimumOrderQuantity().setOriginalValue("3");
+				salesOrg.getSalesOrganizations$MinimumDeliveryQuantity().setOriginalValue("9");
+				salesOrg.setErasable(false);
 
+				org.agito.demo.mdm.material.MaterialBPMOAccess.SalesOrganizations.SalesTexts.Row rowTexts = salesOrg
+						.getSalesOrganizations$SalesTexts().createOriginalRow();
+				rowTexts.getLanguage().setValue("en");
+				rowTexts.getText().setValue("text en");
+				salesOrg.getSalesOrganizations$SalesTexts().addOriginalRow(rowTexts);
+				bpmoAccess.getBPMOData().resetIgnoreControlsOnValueChange();
+			} catch (RuntimeException e) {
+				throw e;
+			} finally {
+				// reset IgnoreControls
+				bpmoAccess.getBPMOData().setIgnoreControlsOnValueChange();
+			}
 			return null;
 		case FindMaterial:
 			// +++ simulate a service interface to find material +++
--- src/test/java/org/agito/demo/mdm/material/test/MaterialTest.java
+++ src/test/java/org/agito/demo/mdm/material/test/MaterialTest.java
@@ -1,10 +1,17 @@
 package org.agito.demo.mdm.material.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.agito.demo.mdm.material.MaterialBPMO;
 import org.agito.demo.mdm.material.MaterialBPMOAccess;
+import org.agito.demo.mdm.material.MaterialBPMOAction;
 import org.agito.demo.mdm.material.MaterialBPMOAccess.SalesOrganizations.SalesTexts;
+import org.agito.demo.mdm.material.MaterialBPMOController.ActionParameter;
 import org.agito.demo.mdm.material.MaterialBPMOLifecycle;
 import org.agito.demo.mdm.material.MaterialBPMOProcessActivity;
+import org.agito.demo.mdm.material.dto.MaterialHeaderDTO;
+import org.agito.demo.mdm.material.dto.MaterialHeaderList;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -74,6 +81,153 @@ public class MaterialTest {
 		access.getName().setValue("Test");
 		access.getMaterialType().setValue("1");
 		access.getBaseUnitOfMeasure().setValue("DZN");
+
+		// start process
+		bpmo.startProcess();
+
+		Assert.assertNotNull(bpmoRule.getRuntimeService().createProcessHistoryQuery()
+				.processInstanceId(bpmo.getBPMOHeader().getProcessInstanceId())
+				.eventType(ProcessAgentEventType.PROCESS_START).singleResult());
+
+		// checkout and complete header task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.HeaderMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete plant task
+		bpmoRule.switchUser("alice");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.PlantMgmt);
+		access.getPlants().createAndAddElement("Berlin").getPlants$ProductionSupervisor().setValue("foobar");
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete storage location task
+		bpmoRule.switchUser("bob");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.StorageMgmt);
+		access.getPlants().getBPMOAccessForNodeElements().get(0).getPlants$StorageLocations()
+				.createAndAddElement("123").getPlants$StorageLocations$StockInQualityInspection().setValue("foo");
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete distribution task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.DistributionMgmt);
+		access = access.getSalesOrganizations().createAndAddElement("DE", "Berlin");
+		access.getSalesOrganizations$StatisticsGroup().setValue("0815");
+		SalesTexts.Row row = access.getSalesOrganizations$SalesTexts().createAndAddRow();
+		row.getLanguage().setValue("foo");
+		row.getText().setValue("bar");
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete approver task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.Approver);
+		bpmo.completeTaskInstance("Approve", null);
+
+		// wait for async jobs
+		bpmoRule.waitForActivitiJobExecutorToProcessAllJobs(30000l, 200l);
+
+		// assert process end
+		Assert.assertNotNull(bpmoRule.getRuntimeService().createProcessHistoryQuery()
+				.bpmoUuid(bpmo.getBPMOHeader().getBPMOUuid()).eventType(ProcessAgentEventType.PROCESS_END)
+				.singleResult());
+
+	}
+
+	@BPMOTestUserId("bob")
+	@Test
+	public void testProcessLifecycleCreateDeclineTask() {
+
+		// create bpmo
+		IBPMO bpmo = bpmoRule.getRuntimeService().createBPMO(MaterialBPMO.$BPMO, MaterialBPMOLifecycle.CREATE, "001");
+
+		// fill bpmo
+		MaterialBPMOAccess access = new MaterialBPMOAccess(bpmo.getBPMOData());
+		access.getName().setValue("Test");
+		access.getMaterialType().setValue("1");
+		access.getBaseUnitOfMeasure().setValue("DZN");
+
+		// start process
+		bpmo.startProcess();
+
+		Assert.assertNotNull(bpmoRule.getRuntimeService().createProcessHistoryQuery()
+				.processInstanceId(bpmo.getBPMOHeader().getProcessInstanceId())
+				.eventType(ProcessAgentEventType.PROCESS_START).singleResult());
+
+		// checkout and complete header task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.HeaderMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete plant task
+		bpmoRule.switchUser("alice");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.PlantMgmt);
+		access.getPlants().createAndAddElement("Berlin").getPlants$ProductionSupervisor().setValue("foobar");
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete storage location task
+		bpmoRule.switchUser("bob");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.StorageMgmt);
+		access.getPlants().getBPMOAccessForNodeElements().get(0).getPlants$StorageLocations()
+				.createAndAddElement("123").getPlants$StorageLocations$StockInQualityInspection().setValue("foo");
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and decline distribution task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.DistributionMgmt);
+		access = access.getSalesOrganizations().createAndAddElement("DE", "Berlin");
+		access.getSalesOrganizations$StatisticsGroup().setValue("0815");
+		SalesTexts.Row row = access.getSalesOrganizations$SalesTexts().createAndAddRow();
+		row.getLanguage().setValue("foo");
+		row.getText().setValue("bar");
+		bpmo.completeTaskInstance("Decline", "Declined");
+
+		// checkout and complete requester task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.Requester);
+		bpmo.completeTaskInstance("Repeat", "is fixed");
+
+		// checkout and complete header task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.HeaderMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete plant task
+		bpmoRule.switchUser("alice");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.PlantMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete storage location task
+		bpmoRule.switchUser("bob");
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.StorageMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete distribution task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.DistributionMgmt);
+		bpmo.completeTaskInstance("Complete", null);
+
+		// checkout and complete approver task
+		bpmo.claimTaskInstance(MaterialBPMOProcessActivity.Approver);
+		bpmo.completeTaskInstance("Approve", null);
+
+		// wait for async jobs
+		bpmoRule.waitForActivitiJobExecutorToProcessAllJobs(30000l, 200l);
+
+		// assert process end
+		Assert.assertNotNull(bpmoRule.getRuntimeService().createProcessHistoryQuery()
+				.bpmoUuid(bpmo.getBPMOHeader().getBPMOUuid()).eventType(ProcessAgentEventType.PROCESS_END)
+				.singleResult());
+
+	}
+	
+	
+	
+	@BPMOTestUserId("bob")
+	//@Test
+	public void testProcessLifecycleUpdate() {
+
+		// create bpmo
+		IBPMO bpmo = bpmoRule.getRuntimeService().createBPMO(MaterialBPMO.$BPMO, MaterialBPMOLifecycle.UPDATE, "001");
+
+		// fill bpmo
+		MaterialBPMOAccess access = new MaterialBPMOAccess(bpmo.getBPMOData());
+		
+		MaterialHeaderDTO material=new MaterialHeaderList().findMaterialByElements(null, 1).get(0);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put(ActionParameter.MATERIAL_HEADER_DTO.toString(), material);
+		access.getBPMO().execute(MaterialBPMOAction.ReadMaterial, parameters);
+	
 
 		// start process
 		bpmo.startProcess();
